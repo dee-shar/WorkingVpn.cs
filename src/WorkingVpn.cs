73cc06a04ab59d34a1c4a52c1b432002c3ab47eb
huok <77536370+dee-shar@users.noreply.github.com>
2025-10-26 03:57:00 +0300

    Update and rename working_vpn.sh to WorkingVpn.cs

---
+++ src/WorkingVpn.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace WorkingVpnApi
+{
+    public class WorkingVpn
+    {
+        private readonly HttpClient httpClient;
+        private readonly string apiUrl = "https://workingvpn.com/api/extension";
+        public WorkingVpn()
+        {
+            httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.UserAgent.ParseAdd(
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36");
+            httpClient.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<string> GetServers()
+        {
+            var response = await httpClient.GetAsync($"{apiUrl}/servers");
+            return await response.Content.ReadAsStringAsync();
+        }
+        public async Task<string> GetStatus()
+        {
+            var response = await httpClient.GetAsync($"{apiUrl}/status");
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
